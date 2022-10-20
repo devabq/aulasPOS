93f53a062a0236dc05b4463cbabff76a892b84e4
Paulo Cezar de Carvalho Albuquerque <oluapcca@gmail.com>
2022-10-20 17:26:28 +0000

    adiconado cr dos users

--- pratica_de_cli_e_crud.py
+++ pratica_de_cli_e_crud.py
@@ -2,61 +2,93 @@ import requests
 
 apiUrl = "https://jsonplaceholder.typicode.com"
 
-##################
+#################
 # Listar usuários
 #################
 
-def option1 ():
+def option1ListUsers ():
     users = apiUrl + "/users/"
     getList = requests.get(users)
-    print(getList.json())
+    return getList.json()
 
 #############################
 #Listar as tarefas do usuário
 #############################
 
-def option2 ():
-    userId = input ("Listar as tarefas de qual usuário? ")
+def option2ListUserTasks ():
+    userId = input ("Listar as tarefas de qual usuário? \n")
     tasksFrom = apiUrl + "/users/" + userId + "/todos/"
     getTasks = requests.get(tasksFrom)
-    print(getTasks.json())
+    return getTasks.json()
 
 ###########
 #CRUD users
 ###########
 
-def option3 ():
+def cUserFun(userInfo):
+    cUserUrl = apiUrl + "/users/"
+    cUser = requests.post(cUserUrl, json=userInfo)
+    return cUser.json()
+
+def rUserFun(userId):
+    rUserUrl = apiUrl + "/users/" + str(userId)
+    rUser = requests.get(rUserUrl)
+    return rUser.json()
+
+def uUserFun(user)
+
+def option3CrudUsers ():
     while True:
-        crudOption = input("Selecione C/R/U/D: ")
-        if optionSelec == "C":
-            createUser()
-        elif optionSelec == "R":
-            readUser()
-        elif optionSelec == "U":
+        crudOption = input("=========\nSelecione C/R/U/D ou B para voltar ao menu principal: \n")
+        if crudOption == "C":
+            userName = input("Digite o nome do usuário: \n")
+            userEmail = input("Digite o email do usuário: \n")
+            userInfo = {"name": userName, "email": userEmail}
+            print(cUserFun(userInfo))
+        elif crudOption == "R":
+            userId = int(input("Digite a ID do usuário a ser lido:\n"))
+            if userId > 10:
+                print("=========\nUsuário não possui informações ou não existe!")
+            elif 0 < userId < 11:
+                print(rUserFun(userId))
+            else:
+                print("=========\nOcorreu um erro!")
+        elif crudOption == "U":
             updateUser()
-        elif optionSelec == "D":
+        elif crudOption == "D":
             deleteUser()
-        else:
+        elif crudOption == "B":
             break
+        else:
+            print("=========\nOpção Inválida!")
+
+###########
+#CRUD tasks
+###########
 
 ####
 #CLI
 ####
 
 while True:
-    optionSelec = int(input("""=========
-    1: Listar usuários;  
-    2: Listar as tarefas do usuário;
-    3: CRUD users;
-    Digite o número da opção desejada: """))
+    optionSelec = input("""=========
+1: Listar usuários;  
+2: Listar as tarefas do usuário;
+3: CRUD users;
+4: CRUD tasks;
+Q: Sair do programa;
+Digite o número da opção desejada: 
+""")
 
-    if optionSelec == 1:
-        option1()
-    elif optionSelec == 2:
-        option2()
-    elif optionSelec == 3:
-        option3()
-    elif optionSelec == 4:
-        option4()
-    else:
+    if optionSelec == "1":
+        print(option1ListUsers())
+    elif optionSelec == "2":
+        print(option2ListUserTasks())
+    elif optionSelec == "3":
+        option3CrudUsers()
+    elif optionSelec == "4":
+        option4CrudTasks()
+    elif optionSelec == "Q":
         break
+    else:
+        print("=========\nOpção inválida!")
